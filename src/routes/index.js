97ac1653b62d994ea991c6560267a088d1e5fc44
Logu R <55403931+Logu-fosablanca@users.noreply.github.com>
2022-07-20 23:19:25 +0530

    Added Get Report

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createDraft } from "../controllers/createDraftController.js";
 import { getDraft } from "../controllers/getDraftController.js";
+import { getReport } from "../controllers/getReportController.js";
 
 
 const router = Router();
 
 router.post('/edi/saveAsDraft',createDraft );
-router.post('edi/getDraft', getDraft);
+router.post('/edi/getDraft', getDraft);
+router.post('/report/get',getReport);
 
 export default router;
